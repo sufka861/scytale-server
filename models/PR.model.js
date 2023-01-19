68767a009da7e0f6f2b92e37e36c0d640880436e
Suf <sufkarmon2@gmail.com>
2023-01-19 12:05:20 +0200

    add validations to PR Model

--- models/PR.model.js
+++ models/PR.model.js
@@ -3,12 +3,20 @@ const {Schema, model, ObjectId} = require("mongoose");
 const PRSchema = new Schema(
         {
             pr_number: {type: Number},
-            title: {type: String},
+            title: {type: String, required: true},
             description: {type: String},
             author: {type: Object},
-            status: {type: String},
+            status: {type: String, required: true},
             labels: {type: [String]},
-            date: {type: String},
+            date: {
+                type: Date,
+                validate: {
+                    validator: (date) => {
+                        return date >= Date.now();
+                    },
+                    message: "Time of PR cannot be prior to this time"
+                }
+            }
         },
         {
             collection: "pull-requests"
