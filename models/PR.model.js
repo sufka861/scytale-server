c07e9f5dd026bc18934c66a12c3c5b00184ba250
Suf <sufkarmon2@gmail.com>
2023-01-19 22:48:41 +0200

    add validations to model

--- models/PR.model.js
+++ models/PR.model.js
@@ -1,18 +1,34 @@
 const {Schema, model, ObjectId} = require("mongoose");
 
 const PRSchema = new Schema(
-        {
-            title: {type: String, required: true},
-            description: {type: String},
-            author: {type: Object, required: true},
-            status: {type: String, required: true},
-            labels: {type: [String]},
-            pr_number: {type: Number, required: true},
-            date: {type: Date, required: true}
+    {
+        title: {type: String, required: true},
+        description: String,
+        author: {
+            type: Object,
+            required: true,
+            properties: {
+                first_name: {type: String, required: true},
+                last_name: {type: String, required: true}
+            }
         },
-        {
-            collection: "pull-requests"
-        }
-    );
+        status: {
+            type: String,
+            required: true,
+            validate: {
+                validator: (status) => {
+                    return (status === "Draft" || status === "Closed" || status === "Open");
+                },
+                message: "Status could only be defines as Draft / Closed / Open",
+            },
+        },
+        labels: [String],
+        pr_number: {type: Number, required: true, min: 1},
+        date: {type: Date, required: true}
+    },
+    {
+        collection: "pull-requests"
+    }
+);
 
 module.exports = model("PRModel", PRSchema);
